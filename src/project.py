1c2bd38f8620c226ef31f3d77f1d4bcd9922fd9e
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2026-07-09 12:30:15 -0300

    dashed lines in total floats for consistency

--- src/project.py
+++ src/project.py
@@ -107,6 +107,8 @@ for s in resources.index:
         data=project.loc[project['Scenario'].isin(scenarios)].groupby(['Scenario', 'Year']).sum(), 
         x='Year', y='Active Floats', hue='Scenario', 
         hue_order=resources.index,
+        size='Scenario', sizes={'Existing Resources':3, 'Scenario A':2, 'Scenario B':2, 'Scenario C':2},
+        style='Scenario', dashes={'Existing Resources':'', 'Scenario A':(2, 1), 'Scenario B':(2, 1), 'Scenario C':(2, 1)},
         marker='o', markersize=8,
         palette='colorblind', ax=ax
     )
